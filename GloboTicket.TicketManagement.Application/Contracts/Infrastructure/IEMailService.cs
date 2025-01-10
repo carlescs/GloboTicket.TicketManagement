972f85c81def3e176296d60aaba92cea546fc5b7
Carles Company Soler <carles.cs@outlook.com>
2025-01-10 23:21:20 +0100

    Add infrastructure project with email service
    Added a new project `GloboTicket.TicketManagement.Infrastructure` targeting `net9.0` with dependencies on `Microsoft.Extensions.Options.ConfigurationExtensions` and `SendGrid`. Introduced `InfrastructureServiceRegistration` for service registration, and implemented `EMailService` using SendGrid to send emails. Created `IEMailService` interface and `EMailSettings` class for configuration. Updated `CreateEventCommandHandler` to send emails on event creation. Updated solution file to include the new project.

---
+++ GloboTicket.TicketManagement.Application/Contracts/Infrastructure/IEMailService.cs
@@ -0,0 +1,8 @@
+﻿using GloboTicket.TicketManagement.Application.Models.Mail;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
+
+public interface IEMailService
+{
+    Task<bool> SendEmail(Email email);
+}
