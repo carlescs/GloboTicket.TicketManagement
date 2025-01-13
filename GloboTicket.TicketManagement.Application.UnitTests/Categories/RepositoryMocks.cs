bcb236a82bad9be1f115b565c6ba9ff469fa6054
Carles Company Soler <carles.cs@outlook.com>
2025-01-13 07:46:37 +0100

    Add unit tests and refactor CreateCategoryCommand
    - Removed unused `using` statements and `CreateCategoryCommandHandler` class from `CreateCategoryCommand.cs`.
    - Updated solution file to include new `GloboTicket.TicketManagement.Application.UnitTests` project.
    - Added `GloboTicket.TicketManagement.Application.UnitTests.csproj` targeting .NET 9.0 with necessary testing NuGet packages.
    - Added `RepositoryMocks.cs` with a mock implementation of `ICategoryRepository`.
    - Added `CreateCategoryCommandHandlerTests.cs` with unit tests for `CreateCategoryCommandHandler`.
    - Added `GetCategoriesListQueryHandlerTests.cs` with a unit test for `GetCategoriesListQueryHandler`.
    - Added `CreateCategoryCommandHandler.cs` with the implementation of `CreateCategoryCommandHandler` class.

---
+++ GloboTicket.TicketManagement.Application.UnitTests/Categories/RepositoryMocks.cs
@@ -0,0 +1,33 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Moq;
+
+namespace GloboTicket.TicketManagement.Application.UnitTests.Categories;
+
+public static class RepositoryMocks
+{
+    public static Mock<ICategoryRepository> GetCategoryRepositoryMock()
+    {
+        List<Category> categories =
+        [
+            new Category
+            {
+                CategoryId = Guid.Parse("{01269212-F0B7-49E3-B9F0-1929AB6B8A5F}"),
+                Name = "Concerts"
+            },
+            new Category
+            {
+                CategoryId = Guid.Parse("{01269212-F0B7-49E3-B9F0-1929AB6B8A52}"),
+                Name = "Musicals"
+            }
+        ];
+        var mock = new Mock<ICategoryRepository>();
+        mock.Setup(repo => repo.ListAllAsync()).ReturnsAsync(categories);
+        mock.Setup(repo => repo.AddAsync(It.IsAny<Category>())).ReturnsAsync((Category category) =>
+        {
+            categories.Add(category);
+            return category;
+        });
+        return mock;
+    }
+}
